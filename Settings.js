9635adc595413ab79e7ee0d8849d0c0a9b73aa3c
Gustavo Tavares <gustavotavares0212@gmail.com>
2024-02-06 15:26:44 -0300

    Sobe arquivos Projeto Portifólio

---
+++ Settings.js
@@ -0,0 +1,24 @@
+{
+  "workbench.colorTheme": "",
+  "editor.fontSize": 16,
+  "editor.lineHeight": 1.75,
+  "editor.tabSize": 2,
+  "workbench.startupEditor": "newUntitledFile",
+  "editor.wordWrap": "on",
+  "editor.minimap.renderCharacters": false,
+  "telemetry.telemetryLevel": "off",
+  "security.workspace.trust.untrustedFiles": "open",
+  "breadcrumbs.filePath": "off",
+  "outline.icons": false,
+  "liveServer.settings.don'tShowInfoMsg": true,
+  "liveServer.settings.don'tVerifyTags": true,
+  "editor.formatOnSave": true,
+  "html.format.wrapAttributes": "auto",
+  "html.format.wrapLineLength": 0,
+  "html.autoClosingTags": false,
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "[html]": {
+    "editor.defaultFormatter": "vscode.html-language-features"
+  }
+  "editor.bracketPairColorization.enabled": false
+}
